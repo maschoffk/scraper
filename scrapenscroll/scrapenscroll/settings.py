86013fecba584f8ad19bc24f24c9fe3dee041de0
LMascher <40897942+LMascher@users.noreply.github.com>
2018-08-16 21:06:12 -0400

    asdf

--- scrapenscroll/scrapenscroll/settings.py
+++ scrapenscroll/scrapenscroll/settings.py
@@ -12,7 +12,7 @@ BOT_NAME = 'scrapenscroll'
 
 SPIDER_MODULES = ['scrapenscroll.spiders']
 NEWSPIDER_MODULE = 'scrapenscroll.spiders'
-logging.warning('started logging in the settings file')
+print('started logging in the settings file')
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
